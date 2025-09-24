84961e475f56783114e4ee9dee6d917e31a67d84
adithya1231 <saichairu@gmail.com>
2025-09-25 00:43:54 +0530

    added my website

---
+++ decisionmaking.py
@@ -0,0 +1,71 @@
+#w=input()
+#if w == "sunny":
+ #   print("we can play cricket")
+#
+#elif w=="cloudy":
+  #  print("we will -lay with the robots")
+#else:
+ #   print("robot")
+#print("code ended here")
+##weather=input("how the weather is:")
+#timeday=input("day/night:")
+##   print("you can play with your car.")
+#elif weather=="rainy":
+ #   print("you can play with your boat today.")
+##   print("you can play with your teddybear toy today.")
+#else:
+#    print("you can play with your snowman.")
+#print("have a great time.")
+#a=int(input("enter a number:"))
+#b=int(input("enter a number:"))
+#operation=input("")
+#if operation=="+":
+ #   print(a+b)
+#elif operation=="-":
+ #   print(a-b)
+#elif operation=="*":
+ #   print(a*b)
+#elif operation== "/":
+ #   print(a/b)
+#else:
+ #   print("give correct value")
+
+#str="PYTHON"
+#print(str[0:4:2])
+#print(str[1])
+#print(str[2])
+#print(str[3])
+#print(str[::-1])
+#a=input("enter the word:")
+#a=a.upper()
+##c=a.count("E")
+#d=a.count("O")
+###print(f"Number of vowels:{b+c+d+f+g}")
+#a=int(input("enter the marks in math:"))
+#b=int(input("enter the marks in Science:"))
+#c=int(input("enter the marks in English:"))
+#total_marks=a+b+c
+#average=float(total_marks/3)
+#print(f"Total Marks:{total_marks}")
+#print(f"Average Marks:{average}")
+#if 80<average<90:
+ #   print("Grade:A")
+#elif 70<=average<=80:
+#    print("Grade:B")
+#else:
+#    print("Grade:C")
+#s=input("enter the palindrome name:")
+#s1=s[::-1]
+#if s==s1:
+ #   print("It is a palindrome.")
+##
+a,b,c=input().split(",")
+a=int(a)
+b=int(b)
+c=int(c)
+if a>b and a>c:
+    print(f"The largest number is {a}.")
+elif b>a and b>c:
+     print(f"The largest number is {b}.")
+else:
+      print(f"The largest number is {c}.")
---
+++ input.py
@@ -0,0 +1,81 @@
+#input statements
+#name=input("enter your name")
+#age=int(input("enter your age"))
+#print("hello,"+name+".you are",age,"years old")#
+#a=int(input("give a value:"))
+#b=int(input("give b value:"))
+#print(a,b,sep=",",end=" Code ends here")
+#a=input("enter your name:")
+#print("hello",a,sep=",",end="!")
+#a=float(input())
+#print("Value of Pi:",a,sep="")
+#a=input()
+#x,y,z=a.split(" ")
+#print(x,y,z)
+#c=int(x)+int(y)+int(z)
+#print(c)
+#a=input()
+#x,y=a.split(",")
+#print("Name:",x,",Age:",y,"",sep="")
+
+#a,b=input().split(" ")
+#a=int(a)
+#b=int(b)
+#10
+#print("Addition:",a+b , "Subtraction:",a-b,"Multipication:",a*b," Division:",a/b, sep="10")
+#a,b=input("enter a and b valuess:").split(",")
+#a=int(a)
+#b=int(b)
+#print("10>5:",10>5,"10<5:",10<5,"10==5:",10==5, "10!=5:",10!=5,sep=" ")
+#a,b=input("enter a and b").split(",")
+#print(a and b,a or b,not b)
+#a,b=input().split(",")
+#b=int(b)
+#print(f"Name:{a},Age:{b} years")
+#a=int(input("num1:"))
+#b=int(input("num2"))
+#print(f"sum:{a+b}")
+#a=int(input("Rafius:"))
+#b=3.14
+#c=float(a*a*b)
+#print(f"Area of the circle:{c}")
+#print(f"Area of the circle:{c}")
+#a=int(input("give a value:"))
+#b=int(input("give b value:"))
+#c=int(input("give c value:"))
+##root1=0
+#oot2=0
+#oot1=(-b+(d**0.5))/2*a
+#root2=(-b-(d**0.5))/2*a
+#print(f"Roots:({root1},{root2})")
+
+#a=int(input("GIve a number"))
+#b=int(input("GIve a number"))
+#temp=a
+#a=b
+#b=temp
+#print(a,b)
+#a+=b
+#b=a-b
+#a=a-b
+#print(a,b)
+#a=int(input("give tempature:"))
+#b=a*9/5+32
+#c=273.15+a
+#print(f"Temperture in fhrenheit:{b}")
+#print(f"Temperture in kelvin:{c}")
+#a=int(input("amount in usd:"))
+#b=float(input("enxchange in eur"))
+#c=a*b
+#print(f"Equivalent amount in EUR:{c}")
+#decision making
+#first_name=input("give your first name:")
+#second_name=input("give your second name:")
+#full_name=first_name+" "+second_name
+#print(full_name)
+#print(len(first_name))
+#s="hello World"
+#print(s.upper())
+#print(s.lower())
+#print(s.replace("o","a"))
+print("hello\n's world")
